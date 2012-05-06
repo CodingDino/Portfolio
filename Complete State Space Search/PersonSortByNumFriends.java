a0d78f0a61789c55b72fcafae16ee3310a09d520
Jiyambi <sarah.ann.herzog@gmail.com>
2012-05-06 12:17:04 -0700

    Changes tabs to spaces

--- Complete State Space Search/PersonSortByNumFriends.java
+++ Complete State Space Search/PersonSortByNumFriends.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 public class PersonSortByNumFriends implements Comparator<Person>{
 
     public int compare(Person a, Person b) {
-		return Math.min(4,b.friends.length) - Math.min(4,a.friends.length);
+        return Math.min(4,b.friends.length) - Math.min(4,a.friends.length);
     }
     
 }
--- Complete State Space Search/PersonSortByPotential.java
+++ Complete State Space Search/PersonSortByPotential.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 public class PersonSortByPotential implements Comparator<Person>{
 
     public int compare(Person a, Person b) {
-		return  b.potential - a.potential;
+        return  b.potential - a.potential;
     }
     
 }
