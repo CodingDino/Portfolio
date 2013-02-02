f0ce7b0dcfcabd8f376192127b28494dc3650640
Sarah Herzog <sarah.ann.herzog@gmail.com>
2013-02-02 14:38:40 +0000

    Removes state space search project

--- Complete State Space Search/PersonSortByNumFriends.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class PersonSortByNumFriends implements Comparator<Person>{
-
-    public int compare(Person a, Person b) {
-        return Math.min(4,b.friends.length) - Math.min(4,a.friends.length);
-    }
-    
-}
--- Complete State Space Search/PersonSortByPotential.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class PersonSortByPotential implements Comparator<Person>{
-
-    public int compare(Person a, Person b) {
-        return  b.potential - a.potential;
-    }
-    
-}
